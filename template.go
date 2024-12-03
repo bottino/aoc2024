09c2fb6efe83d5d168a31e5e1192e4b46d4cc832
Baptiste Ottino <baptiste.ottino@gmail.com>
2024-12-03 23:09:16 +0100

    Add template

---
+++ template.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+//go:embed example.txt
+var example string
+
+//go:embed input.txt
+var input string
+
+func init() {
+	example = strings.TrimRight(example, "\n")
+	input = strings.TrimRight(input, "\n")
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "p", 1, "The part of the puzzle")
+	var useExample bool
+	flag.BoolVar(&useExample, "e", false, "Use the example as input")
+	flag.Parse()
+
+	if useExample {
+		input = example
+	}
+
+	var solution int
+	if part == 1 {
+		solution = part1(input)
+	} else {
+		solution = part2(input)
+	}
+
+	fmt.Println(solution)
+}
+
+func part1(input string) int {
+	// part 1 here
+	return 1
+}
+
+func part2(input string) int {
+	// part 2 here
+	return 2
+}
