1fbddcb84db0c91ad4238c4f25066169257adbee
Baptiste Ottino <baptiste.ottino@gmail.com>
2024-12-03 22:51:29 +0100

    Day 01: Better interface

--- 01/main.go
+++ 01/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	_ "embed"
+	"flag"
 	"fmt"
-	"log"
 	"regexp"
 	"slices"
 	"strconv"
@@ -17,23 +17,48 @@ var example string
 var input string
 
 func init() {
-	example = strings.TrimRight(input, "\n")
+	example = strings.TrimRight(example, "\n")
 	input = strings.TrimRight(input, "\n")
 }
 
 func main() {
+	var part int
+	flag.IntVar(&part, "p", 1, "The part of the puzzle")
+	var useExample bool
+	flag.BoolVar(&useExample, "e", false, "Use the example as input")
+	flag.Parse()
+
+	if useExample {
+		input = example
+	}
+
+	var solution int
+	if part == 1 {
+		solution = part1(input)
+	} else {
+		solution = part2(input)
+	}
+
+	fmt.Println(solution)
+}
+
+func part1(input string) int {
 	var left []int
 	var right []int
 	for _, line := range strings.Split(input, "\n") {
 		re := regexp.MustCompile(`(\d+)\s+(\d+)`)
 		matches := re.FindStringSubmatch(line)
 		if len(matches) > 0 {
-			left_value, _ := strconv.Atoi(matches[1])
-			right_value, _ := strconv.Atoi(matches[2])
+			left_value, err_l := strconv.Atoi(matches[1])
+			right_value, err_r := strconv.Atoi(matches[2])
+			if err_l != nil || err_r != nil {
+				panic("String conversion failed")
+			}
+
 			left = append(left, left_value)
 			right = append(right, right_value)
 		} else {
-			log.Fatal("No match found")
+			panic("No match found")
 		}
 	}
 
@@ -44,7 +69,13 @@ func main() {
 	for i := range left {
 		total_distance += absInt(left[i] - right[i])
 	}
-	fmt.Println(total_distance)
+
+	return total_distance
+}
+
+func part2(input string) int {
+	fmt.Println("This will be part 2")
+	return 0
 }
 
 func absInt(x int) int {
