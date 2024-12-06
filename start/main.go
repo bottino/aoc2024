b73f745fa79d4446d00e00a1ce450d9cb8d926f3
Baptiste Ottino <baptiste.ottino@gmail.com>
2024-12-06 12:18:40 +0100

    Better handling of errors

--- start/main.go
+++ start/main.go
@@ -18,12 +18,20 @@ func main() {
 	now := time.Now()
 	var dayNumber int
 	flag.IntVar(&dayNumber, "d", now.Day(), "Advent of code day")
+	flag.Parse()
 
 	dayName := fmt.Sprintf("day%02d", dayNumber)
+
+	// Exist if dir already exists
+	if stat, err := os.Stat(dayName); err == nil && stat.IsDir() {
+		fmt.Printf("Directory for day %d already created.\n", dayNumber)
+		return
+	}
 
 	err := os.Mkdir(dayName, 0755)
 	if err != nil {
 		fmt.Printf("Error creating directory for day %d: %v", dayNumber, err)
+		return
 	}
 
 	filePath := dayName + "/" + dayName + ".go"
