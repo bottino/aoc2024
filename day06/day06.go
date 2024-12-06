97fdd350d0735b4bb3bdcfd74e129131325097bb
Baptiste Ottino <baptiste.ottino@gmail.com>
2024-12-06 13:09:27 +0100

    Day 05: Part 1 - refactor with Coord

--- day06/day06.go
+++ day06/day06.go
@@ -9,20 +9,20 @@ func Part1(input string) (solution int) {
 	grid, guard := readInput(input)
 	solution++ // the first square is visited
 	for {
-		x, y := guard.X+guard.D[0], guard.Y+guard.D[1]
-		newSquare, ok := grid[x][y]
+		pos := Coord{guard.Pos[0] + guard.Dir[0], guard.Pos[1] + guard.Dir[1]}
+		newSquare, ok := grid[pos]
 		if ok == false {
-			fmt.Println("Guard exited at", x, y)
+			fmt.Println("Guard exited at", pos)
 			break
 		}
 
 		switch newSquare {
 		case '.':
-			grid[x][y] = 'X'
-			guard.X, guard.Y = x, y
+			grid[pos] = 'X'
+			guard.Pos = pos
 			solution++
 		case 'X':
-			guard.X, guard.Y = x, y
+			guard.Pos = pos
 		case '#':
 			guard.TurnRight()
 		}
@@ -36,55 +36,55 @@ func Part2(input string) (solution int) {
 	return
 }
 
-type Dir [2]int
+type Coord [2]int
 
 var (
-	up    = Dir{-1, 0}
-	down  = Dir{1, 0}
-	left  = Dir{0, -1}
-	right = Dir{0, 1}
+	up    = Coord{-1, 0}
+	down  = Coord{1, 0}
+	left  = Coord{0, -1}
+	right = Coord{0, 1}
 )
 
 type Guard struct {
-	X, Y int
-	D    Dir
+	Pos Coord
+	Dir Coord
 }
 
 func (g *Guard) TurnRight() {
-	switch g.D {
+	switch g.Dir {
 	case up:
-		g.D = right
+		g.Dir = right
 	case right:
-		g.D = down
+		g.Dir = down
 	case down:
-		g.D = left
+		g.Dir = left
 	case left:
-		g.D = up
+		g.Dir = up
 	}
 }
 
-type Grid map[int]map[int]rune
+type Grid map[Coord]rune
 
 func readInput(input string) (grid Grid, guard Guard) {
 	grid = make(Grid)
 	for i, line := range strings.Split(input, "\n") {
-		grid[i] = make(map[int]rune)
 		for j, char := range line {
+			square := Coord{i, j}
 			switch char {
 			case '^':
-				guard = Guard{i, j, up}
-				grid[i][j] = 'X'
+				guard = Guard{square, up}
+				grid[square] = 'X'
 			case '>':
-				guard = Guard{i, j, right}
-				grid[i][j] = 'X'
+				guard = Guard{square, right}
+				grid[square] = 'X'
 			case '<':
-				guard = Guard{i, j, left}
-				grid[i][j] = 'X'
+				guard = Guard{square, left}
+				grid[square] = 'X'
 			case 'v':
-				guard = Guard{i, j, down}
-				grid[i][j] = 'X'
+				guard = Guard{square, down}
+				grid[square] = 'X'
 			default:
-				grid[i][j] = char
+				grid[square] = char
 			}
 		}
 	}
