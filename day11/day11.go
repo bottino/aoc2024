79884cd96cb8b79192f18bf7d2ec87aba00a1701
Baptiste Ottino <baptiste.ottino@gmail.com>
2024-12-13 10:23:24 +0100

    Day 11: Part 2

--- day11/day11.go
+++ day11/day11.go
@@ -1,66 +1,73 @@
 package day11
 
 import (
-	"fmt"
 	"math"
 	"strconv"
 	"strings"
 )
 
 func Part1(input string) int {
+	var numStones int
 	stones := buildStoneMap(input)
 	for i := 0; i < 25; i++ {
-		newStones := blink(stones)
-		stones = append(stones, newStones...)
+		stones, numStones = blink(stones)
 	}
 
-	return len(stones)
+	return numStones
 }
 
-func Part2(input string) (solution int) {
+func Part2(input string) int {
+	var numStones int
 	stones := buildStoneMap(input)
 	for i := 0; i < 75; i++ {
-		newStones := blink(stones)
-		stones = append(stones, newStones...)
-		fmt.Println(i+1, len(stones))
+		stones, numStones = blink(stones)
 	}
 
-	return len(stones)
+	return numStones
 }
 
 type Line map[int]int
 
-func blink(stones []int) (newStones []int) {
-	for i, v := range stones {
+func blink(stones Line) (newStones Line, numStones int) {
+	newStones = make(Line, len(stones)*2)
+	for k, v := range stones {
 
-		if v == 0 {
-			stones[i] = 1
+		if k == 0 {
+			add(1, v, newStones)
+			numStones += v
 			continue
 		}
 
-		n := numDigits(v)
+		n := int(math.Log10(float64(k))) + 1
 		if n%2 == 0 {
-			a := int(float64(v) / math.Pow10(n/2))
-			b := int(float64(v) - math.Pow10(n/2)*float64(a))
-			stones[i] = a
-			newStones = append(newStones, b)
+			a := int(float64(k) / math.Pow10(n/2))
+			b := int(float64(k) - math.Pow10(n/2)*float64(a))
+			add(a, v, newStones)
+			add(b, v, newStones)
+			numStones += 2 * v
 			continue
 		}
 
-		stones[i] = 2024 * v
+		add(2024*k, v, newStones)
+		numStones += v
 	}
 
-	return newStones
+	return newStones, numStones
 }
 
-func numDigits(n int) int {
-	return int(math.Log10(float64(n))) + 1
+func add(k int, v int, m Line) {
+	if _, ok := m[k]; ok {
+		m[k] += v
+	} else {
+		m[k] = v
+	}
 }
 
-func buildStoneMap(input string) (stones []int) {
+func buildStoneMap(input string) (stones Line) {
+	stones = make(Line)
 	for _, s := range strings.Split(input, " ") {
 		d, _ := strconv.Atoi(s)
-		stones = append(stones, d)
+		add(d, 1, stones)
 	}
 
 	return stones
