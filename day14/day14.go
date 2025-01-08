a3548763587ec5457d0fa2ac5d7150f2f489a89f
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-01-08 19:18:09 +0100

    Day 14: Part 1

--- day14/day14.go
+++ day14/day14.go
@@ -10,8 +10,26 @@ import (
 )
 
 func Part1(input string) any {
-	fmt.Println("No solution yet for day 14, part 1")
-	return 0
+	robots := readRobots(input, 103, 101)
+	for i := 0; i < 100; i++ {
+		for _, r := range robots {
+			r.Move()
+		}
+	}
+
+	quad := map[int]int{0: 0, 1: 0, 2: 0, 3: 0}
+	for _, r := range robots {
+		if q := r.Quadrant(); q >= 0 {
+			quad[r.Quadrant()] += 1
+		}
+	}
+
+	safetyFactor := 1
+	for _, v := range quad {
+		safetyFactor *= v
+	}
+
+	return safetyFactor
 }
 
 func Part2(input string) any {
@@ -20,11 +38,44 @@ func Part2(input string) any {
 }
 
 type Robot struct {
-	Pos vec.Coord
-	Dir vec.Coord
+	Pos  vec.Coord
+	Dir  vec.Coord
+	N, M int
 }
 
-func readRobots(input string) (robots []Robot) {
+func (r *Robot) Move() {
+	p := r.Pos.Add(r.Dir)
+	if p.X >= r.N {
+		p.X -= r.N
+	}
+	if p.X < 0 {
+		p.X += r.N
+	}
+	if p.Y >= r.M {
+		p.Y -= r.M
+	}
+	if p.Y < 0 {
+		p.Y += r.M
+	}
+	r.Pos = p
+}
+
+func (r *Robot) Quadrant() int {
+	switch {
+	case r.Pos.X < r.N/2 && r.Pos.Y < r.M/2:
+		return 0
+	case r.Pos.X > r.N/2 && r.Pos.Y < r.M/2:
+		return 1
+	case r.Pos.X < r.N/2 && r.Pos.Y > r.M/2:
+		return 2
+	case r.Pos.X > r.N/2 && r.Pos.Y > r.M/2:
+		return 3
+	default:
+		return -1
+	}
+}
+
+func readRobots(input string, N, M int) (robots []*Robot) {
 	re := regexp.MustCompile(`-?\d+`)
 	for _, line := range strings.Split(input, "\n") {
 		matches := re.FindAllString(line, -1)
@@ -38,8 +89,12 @@ func readRobots(input string) (robots []Robot) {
 		}
 
 		// Reverse x and y because of the convention of the vec package
-		r := Robot{vec.Coord{nums[1], nums[0]}, vec.Coord{nums[3], nums[2]}}
-		robots = append(robots, r)
+		r := Robot{
+			vec.Coord{nums[1], nums[0]},
+			vec.Coord{nums[3], nums[2]},
+			N, M,
+		}
+		robots = append(robots, &r)
 	}
 
 	return robots
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -12,14 +12,32 @@ func TestReadRobots(t *testing.T) {
 p=6,3 v=-1,-3
 p=10,3 v=-1,2`
 
-	robots := readRobots(input)
-	expected := []Robot{
-		{vec.Coord{4, 0}, vec.Coord{-3, 3}},
-		{vec.Coord{3, 6}, vec.Coord{-3, -1}},
-		{vec.Coord{3, 10}, vec.Coord{2, -1}},
+	robots := readRobots(input, 7, 11)
+	expected := []*Robot{
+		{vec.Coord{4, 0}, vec.Coord{-3, 3}, 7, 11},
+		{vec.Coord{3, 6}, vec.Coord{-3, -1}, 7, 11},
+		{vec.Coord{3, 10}, vec.Coord{2, -1}, 7, 11},
 	}
 
 	if diff := cmp.Diff(expected, robots); diff != "" {
 		t.Errorf("Unexpected diff in robots: %s", diff)
+	}
+}
+
+func TestMoveRobot(t *testing.T) {
+	r := Robot{vec.Coord{4, 2}, vec.Coord{-3, 2}, 7, 11}
+	exp := []vec.Coord{
+		{1, 4},
+		{5, 6},
+		{2, 8},
+		{6, 10},
+		{3, 1},
+	}
+
+	for i := 0; i < 5; i++ {
+		r.Move()
+		if diff := cmp.Diff(exp[i], r.Pos); diff != "" {
+			t.Errorf("Wrong pos at time %d: %s", i+1, diff)
+		}
 	}
 }
