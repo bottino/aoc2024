b326cd15e575656727b79cc9e9516a64699a332c
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-01-10 15:26:13 +0100

    Day 25

--- day25/day25.go
+++ day25/day25.go
@@ -1,13 +1,58 @@
 package day25
 
-import "fmt"
+import (
+	"strings"
+)
 
 func Part1(input string) any {
-	fmt.Println("No solution yet for day 25, part 1")
-	return 0
+	keys, locks := readInput(input)
+
+	var fits int
+	for _, k := range keys {
+		for _, l := range locks {
+			var pinFits int
+			for i := 0; i < 5; i++ {
+				if k[i]+l[i] <= 5 {
+					pinFits++
+				}
+			}
+			if pinFits == 5 {
+				fits++
+			}
+		}
+	}
+
+	return fits
 }
 
 func Part2(input string) any {
-	fmt.Println("No solution yet for day 25, part 2")
-	return 0
+	return "Done!"
 }
+
+func readInput(input string) (keys []pins, locks []pins) {
+	for _, pattern := range strings.Split(input, "\n\n") {
+		heights := pins{-1, -1, -1, -1, -1}
+		var isKey bool
+		for i, line := range strings.Split(pattern, "\n") {
+			if i == 0 && line == "#####" {
+				isKey = true
+			}
+
+			for j, char := range line {
+				if char == '#' {
+					heights[j]++
+				}
+			}
+		}
+
+		if isKey {
+			keys = append(keys, heights)
+		} else {
+			locks = append(locks, heights)
+		}
+	}
+
+	return keys, locks
+}
+
+type pins [5]int
