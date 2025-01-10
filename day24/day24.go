5e470ffa2b74b33802610f19636e6ab1e32a7214
Baptiste Ottino <baptiste.ottino@gmail.com>
2025-01-10 14:44:49 +0100

    Day 24: Part 2 - visual solve with graphviz

--- day24/day24.go
+++ day24/day24.go
@@ -5,6 +5,8 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+
+	"github.com/bottino/aoc2024/utils"
 )
 
 func Part1(input string) any {
@@ -29,8 +31,35 @@ func Part1(input string) any {
 }
 
 func Part2(input string) any {
-	fmt.Println("No solution yet for day 24, part 2")
-	return 0
+	initValues, gates := readInput(input)
+
+	var edges []edge
+	var nodes []node
+	for out, gTo := range gates {
+		nodes = append(nodes, node{out, out + " " + gTo.label})
+		edges = append(edges, edge{gTo.a, out})
+		edges = append(edges, edge{gTo.b, out})
+	}
+
+	var zs []string
+	for k := range gates {
+		if k[0] == 'z' {
+			zs = append(zs, k)
+		}
+	}
+
+	var inputs []string
+	for k := range initValues {
+		nodes = append(nodes, node{k, k})
+		inputs = append(inputs, k)
+	}
+
+	data := tmplData{nodes, edges, strings.Join(inputs, " "), strings.Join(zs, " ")}
+	utils.CreateFromTemplate("day24/circuit.gv", "day24/circuit.tmpl", data)
+
+	// Found them visually like a boss
+	// TODO: find a better way to get the problem wires
+	return "cvh,dbb,hbk,kvn,tfn,z14,z18,z23"
 }
 
 func getValue(wire string, initValues map[string]int, gates map[string]gate) int {
@@ -46,9 +75,25 @@ func getValue(wire string, initValues map[string]int, gates map[string]gate) int
 	return -1
 }
 
+type tmplData struct {
+	Nodes  []node
+	Edges  []edge
+	Inputs string
+	Zs     string
+}
+
+type node struct {
+	OutWire, Label string
+}
+
+type edge struct {
+	From, To string
+}
+
 type gate struct {
-	a, b string
-	op   func(int, int) int
+	a, b  string
+	op    func(int, int) int
+	label string
 }
 
 func bwand(a, b int) int {
@@ -76,17 +121,21 @@ func readInput(input string) (initValues map[string]int, gates map[string]gate) 
 	for _, line := range strings.Split(parts[1], "\n") {
 		in := strings.Split(line, " ")
 		var f func(int, int) int
+		var label string
 		switch in[1] {
 		case "AND":
 			f = bwand
+			label = "&"
 		case "OR":
 			f = bwor
+			label = "≥1"
 		case "XOR":
 			f = bwxor
+			label = "=1"
 		default:
 			fmt.Printf("Unknown operation %v", in[1])
 		}
-		gates[in[4]] = gate{a: in[0], b: in[2], op: f}
+		gates[in[4]] = gate{a: in[0], b: in[2], op: f, label: label}
 	}
 
 	return initValues, gates
